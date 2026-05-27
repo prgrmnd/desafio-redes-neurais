dfc2a115b49d4636302950090e7c01d401138026
Josué Serejo <josueserejo5@gmail.com>
2026-05-27 08:30:48 -0300

    criando pesos aleatórios entre 0 e 1
    Co-authored-by: prgrmnd <serejodev@gmail.com>

--- neuronio_simples.py
+++ neuronio_simples.py
@@ -1,4 +1,10 @@
+import random
+
 valor1 = float(input("Digite o primeiro valor: "))
 valor2 = float(input("Digite o segundo valor: "))
 valor3 = float(input("Digite o terceiro valor: "))
+
+peso1 = round(random.uniform(0, 1), 3)
+peso2 = round(random.uniform(0, 1), 3)
+peso3 = round(random.uniform(0, 1), 3)
 
