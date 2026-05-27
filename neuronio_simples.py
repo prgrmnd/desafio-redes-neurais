78c5826666d19c4972c3509e98155d57914a0f22
Josué Serejo <josueserejo5@gmail.com>
2026-05-27 16:45:32 -0300

    Neuronio corrigido

--- neuronio_simples.py
+++ neuronio_simples.py
@@ -7,22 +7,19 @@ valor3 = float(input("Digite o terceiro valor: "))
 peso1 = round(random.uniform(0, 1), 3)
 peso2 = round(random.uniform(0, 1), 3)
 peso3 = round(random.uniform(0, 1), 3)
+peso_limiar = round(random.uniform(0, 1), 3)
 
-lim_atv = -1
+soma_produtos = (valor1 * peso1) + (valor2 * peso2) + (valor3 * peso3) + (-1 * peso_limiar)
 
-soma_produtos = (valor1 * peso1) + (valor2 * peso2) + (valor3 * peso3)
-
-pot_de_atv = round((lim_atv + soma_produtos),3)
-
-if pot_de_atv >= 0:
-    y = 1
+if soma_produtos >= 0:
+    sinal_saida = 1
 else:
-    y = -1
+    sinal_saida = -1
 
-print("\n--- Processamento do Perceptron ---")
+print("\nProcessamento do Perceptron")
 print(f"Valores: [{valor1}, {valor2}, {valor3}]")
-print(f"Pesos:   [{peso1}, {peso2}, {peso3}]")
+print(f"Pesos:   [{peso1}, {peso2}, {peso3}, {peso_limiar}]")
+print(f"Peso do Limiar (Bias): {peso_limiar}")
 print("-" * 40)
-print(f"Soma Linear (Σ):            {round(soma_produtos, 3)}")
-print(f"Potencial de Ativação (u):  {pot_de_atv}")
-print(f"Sinal de Saída Final (y):   {y}")
+print(f"Potencial de Ativação:  {round(soma_produtos, 3)}")
+print(f"Sinal de Saída Final:   {sinal_saida}")
