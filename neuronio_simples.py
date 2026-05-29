55e284f29c37de91be3f4970824db6fe23e7e705
Josué Serejo <josueserejo5@gmail.com>
2026-05-29 16:28:26 -0300

    Adicionando pesos em um dicionário
    Co-authored-by pgrmnd <serejodev@gmail.com>

--- neuronio_simples.py
+++ neuronio_simples.py
@@ -1,13 +1,13 @@
 import random
 
-valor1 = float(input("Digite o primeiro valor: "))
-valor2 = float(input("Digite o segundo valor: "))
-valor3 = float(input("Digite o terceiro valor: "))
-
 peso1 = round(random.uniform(0, 1), 3)
 peso2 = round(random.uniform(0, 1), 3)
 peso3 = round(random.uniform(0, 1), 3)
 peso_limiar = round(random.uniform(0, 1), 3)
+
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+valor3 = float(input("Digite o terceiro valor: "))
 
 soma_produtos = (valor1 * peso1) + (valor2 * peso2) + (valor3 * peso3) + (-1 * peso_limiar)
 
@@ -19,7 +19,7 @@ else:
 print("\nProcessamento do Perceptron")
 print(f"Valores: [{valor1}, {valor2}, {valor3}]")
 print(f"Pesos:   [{peso1}, {peso2}, {peso3}, {peso_limiar}]")
-print(f"Peso do Limiar (Bias): {peso_limiar}")
+print(f"Peso do Limiar: {peso_limiar}")
 print("-" * 40)
 print(f"Potencial de Ativação:  {round(soma_produtos, 3)}")
 print(f"Sinal de Saída Final:   {sinal_saida}")
---
+++ perceptron.py
@@ -0,0 +1,9 @@
+import random
+
+pesos = {
+    'peso1': round(random.uniform(0, 1), 3),
+    'peso2': round(random.uniform(0, 1), 3),
+    'peso3': round(random.uniform(0, 1), 3),
+    'peso_limiar': round(random.uniform(0, 1), 3)
+}
+
